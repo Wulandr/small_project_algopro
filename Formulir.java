9dde024fa41b85c9f40505fd57ef5a27eb4d3643
Wulandr <58412070+Wulandr@users.noreply.github.com>
2019-12-09 15:18:03 +0700

    nambah jenjang dan nampilin data sementara
    akun login, pilihan jenjang, cetak nama, username, password dan jenjang.

--- Formulir.java
+++ Formulir.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Formulir
 {
-    static 
+	static int menu, index = 0;
     static Scanner input = new Scanner(System.in);
     /*
     void login (String user, String pass)
@@ -11,16 +11,25 @@ public class Formulir
     }
     */
 
-    static void pilihProgram ()
+    static void pilihProgram (String prodi[])
     {
         //moves up text/clear screen
-        for (int i = 0; i < 50; ++i) System.out.println();
+        //for (int i = 0; i < 50; ++i) System.out.println();
         System.out.println("====================================");
         System.out.println("Pilih jenjang perkuliahan: ");
         System.out.println("1. S1");
         System.out.println("2. S2");
         System.out.println("3. S3");
-
+        menu = input.nextInt();
+        switch (menu)
+        {
+        case 1 : 
+    		prodi[index] = "S1";
+        case 2 : 
+    		prodi[index] = "S2";
+        case 3 : 
+    		prodi[index] = "S3";
+        }
     } 
     public static void main (String args[])
     {
@@ -34,8 +43,9 @@ public class Formulir
         String sekolah[] = new String[10];
         String ortu[] = new String[10];
         String alamat[] = new String[10];
+        String prodi[] = new String[10];
         
-        int index = 0;
+        //int index = 0;
         System.out.println("Selamat datang, silakan isi formulir");
         System.out.println("====================================");
         System.out.print("Masukkan nama\t\t: ");
@@ -45,12 +55,13 @@ public class Formulir
         System.out.print("Masukkan password\t: ");
         pass[index] = input.nextLine();
         
-        pilihProgram();
-        /*
+        pilihProgram(prodi);
+        
+        
         System.out.println(nama[index]);
         System.out.println(user[index]);
         System.out.println(pass[index]);
-        */
+        System.out.println(prodi[index]);
 
     }
 }
